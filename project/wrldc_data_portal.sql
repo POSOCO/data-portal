fb91fd9a3966448e2e8954062c3888e52b7a5c17
nagasudhirpulla <nagasudhirpulla@gmail.com>
2017-04-05 21:18:33 +0530

    added state users

--- project/wrldc_data_portal.sql
+++ project/wrldc_data_portal.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 05, 2017 at 08:08 AM
+-- Generation Time: Apr 05, 2017 at 03:45 PM
 -- Server version: 5.7.9
 -- PHP Version: 5.6.16
 
@@ -43,15 +43,15 @@ CREATE TABLE IF NOT EXISTS `const_data` (
 --
 
 INSERT INTO `const_data` (`id`, `time`, `key_string`, `value_string`, `created_at`, `updated_at`) VALUES
-(15, '2017-04-03 00:00:00', 'kstps_tot_mu', '34', '2017-04-04 14:26:23', '2017-04-04 20:59:29'),
-(16, '2017-04-03 00:00:00', 'sipat_tot_mu', '4554', '2017-04-04 14:26:23', '2017-04-04 20:59:29'),
-(17, '2017-04-03 00:00:00', 'vstps_tot_mu', '41554', '2017-04-04 14:26:23', '2017-04-04 20:59:29'),
-(18, '2017-04-03 00:00:00', 'kawas_tot_mu', '8487', '2017-04-04 14:26:23', '2017-04-04 20:59:29'),
-(19, '2017-04-03 00:00:00', 'gandhar_tot_mu', '4534', '2017-04-04 14:26:23', '2017-04-04 20:59:29'),
-(20, '2017-04-03 00:00:00', 'mouda_tot_mu', '4354', '2017-04-04 14:26:23', '2017-04-04 20:59:29'),
-(81, '2017-04-03 00:00:00', 'ssp_tot_mu', '246', '2017-04-04 15:11:42', '2017-04-04 15:11:42'),
-(88, '2017-04-04 00:00:00', 'kstps_tot_mu', '150', '2017-04-05 00:08:13', '2017-04-05 00:08:13'),
-(89, '2017-04-04 00:00:00', 'sipat_tot_mu', '200', '2017-04-05 00:08:13', '2017-04-05 00:08:13');
+  (15, '2017-04-03 00:00:00', 'kstps_tot_mu', '34', '2017-04-04 14:26:23', '2017-04-04 20:59:29'),
+  (16, '2017-04-03 00:00:00', 'sipat_tot_mu', '4554', '2017-04-04 14:26:23', '2017-04-04 20:59:29'),
+  (17, '2017-04-03 00:00:00', 'vstps_tot_mu', '41554', '2017-04-04 14:26:23', '2017-04-04 20:59:29'),
+  (18, '2017-04-03 00:00:00', 'kawas_tot_mu', '8487', '2017-04-04 14:26:23', '2017-04-04 20:59:29'),
+  (19, '2017-04-03 00:00:00', 'gandhar_tot_mu', '4534', '2017-04-04 14:26:23', '2017-04-04 20:59:29'),
+  (20, '2017-04-03 00:00:00', 'mouda_tot_mu', '4354', '2017-04-04 14:26:23', '2017-04-04 20:59:29'),
+  (81, '2017-04-03 00:00:00', 'ssp_tot_mu', '246', '2017-04-04 15:11:42', '2017-04-04 15:11:42'),
+  (88, '2017-04-04 00:00:00', 'kstps_tot_mu', '150', '2017-04-05 00:08:13', '2017-04-05 00:08:13'),
+  (89, '2017-04-04 00:00:00', 'sipat_tot_mu', '200', '2017-04-05 00:08:13', '2017-04-05 00:08:13');
 
 -- --------------------------------------------------------
 
@@ -76,32 +76,32 @@ CREATE TABLE IF NOT EXISTS `key_strings` (
 --
 
 INSERT INTO `key_strings` (`key_str`, `users_id`, `type_info`, `description`, `created_at`, `updated_at`) VALUES
-('balco_tot_mu', 50, 'number', 'BALCO Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('cgpl_tot_mu', 36, 'number', 'CGPL Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('dgen_tot_mu', 47, 'number', 'DGEN Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('essarmahan_tot_mu', 51, 'number', 'ESSARMAHAN Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('gandhar_tot_mu', 32, 'number', 'Gandhar Generation MU', '2017-04-05 00:12:43', '2017-04-05 00:12:43'),
-('jhabua_tot_mu', 49, 'number', 'JHABUA Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('jpltamnar_tot_mu', 46, 'number', 'JPLTAMNAR Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('jpl_tot_mu', 40, 'number', 'JPL Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('kaps_tot_mu', 34, 'number', 'KAPS Generation MU', '2017-04-05 00:14:36', '2017-04-05 00:14:36'),
-('kawas_tot_mu', 32, 'number', 'Kawas Generation MU', '2017-04-05 00:12:43', '2017-04-05 00:12:43'),
-('ksk_tot_mu', 41, 'number', 'KSK Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('kstps_tot_mu', 32, 'number', 'KSTPS Generation MU', '2017-04-04 23:54:44', '2017-04-04 23:55:43'),
-('lanco_tot_mu', 39, 'number', 'LANCO Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('mbpower_tot_mu', 44, 'number', 'MBPOWER Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('mouda_tot_mu', 32, 'number', 'Mouda Generation MU', '2017-04-05 00:12:43', '2017-04-05 00:12:43'),
-('nspcl_tot_mu', 38, 'number', 'NSPCL Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('rgppl_tot_mu', 48, 'number', 'RGPPL Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('rkm_tot_mu', 42, 'number', 'RKM Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('sasan_tot_mu', 37, 'number', 'SASAN Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('sipat_tot_mu', 32, 'number', 'Sipat Generation MU', '2017-04-04 23:54:44', '2017-04-04 23:54:44'),
-('sks_tot_mu', 45, 'number', 'SKS Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('ssp_tot_mu', 33, 'number', 'SSP Generation MU', '2017-04-05 00:14:36', '2017-04-05 00:14:36'),
-('taps_tot_mu', 35, 'number', 'TAPS Generation MU', '2017-04-05 00:19:26', '2017-04-05 00:19:26'),
-('trn_tot_mu', 43, 'number', 'TRN Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('vandana_tot_mu', 52, 'number', 'VANDANA Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
-('vstps_tot_mu', 32, 'number', 'VSTPS Generation MU', '2017-04-05 00:12:43', '2017-04-05 00:12:43');
+  ('balco_tot_mu', 50, 'number', 'BALCO Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('cgpl_tot_mu', 36, 'number', 'CGPL Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('dgen_tot_mu', 47, 'number', 'DGEN Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('essarmahan_tot_mu', 51, 'number', 'ESSARMAHAN Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('gandhar_tot_mu', 32, 'number', 'Gandhar Generation MU', '2017-04-05 00:12:43', '2017-04-05 00:12:43'),
+  ('jhabua_tot_mu', 49, 'number', 'JHABUA Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('jpltamnar_tot_mu', 46, 'number', 'JPLTAMNAR Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('jpl_tot_mu', 40, 'number', 'JPL Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('kaps_tot_mu', 34, 'number', 'KAPS Generation MU', '2017-04-05 00:14:36', '2017-04-05 00:14:36'),
+  ('kawas_tot_mu', 32, 'number', 'Kawas Generation MU', '2017-04-05 00:12:43', '2017-04-05 00:12:43'),
+  ('ksk_tot_mu', 41, 'number', 'KSK Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('kstps_tot_mu', 32, 'number', 'KSTPS Generation MU', '2017-04-04 23:54:44', '2017-04-04 23:55:43'),
+  ('lanco_tot_mu', 39, 'number', 'LANCO Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('mbpower_tot_mu', 44, 'number', 'MBPOWER Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('mouda_tot_mu', 32, 'number', 'Mouda Generation MU', '2017-04-05 00:12:43', '2017-04-05 00:12:43'),
+  ('nspcl_tot_mu', 38, 'number', 'NSPCL Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('rgppl_tot_mu', 48, 'number', 'RGPPL Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('rkm_tot_mu', 42, 'number', 'RKM Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('sasan_tot_mu', 37, 'number', 'SASAN Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('sipat_tot_mu', 32, 'number', 'Sipat Generation MU', '2017-04-04 23:54:44', '2017-04-04 23:54:44'),
+  ('sks_tot_mu', 45, 'number', 'SKS Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('ssp_tot_mu', 33, 'number', 'SSP Generation MU', '2017-04-05 00:14:36', '2017-04-05 00:14:36'),
+  ('taps_tot_mu', 35, 'number', 'TAPS Generation MU', '2017-04-05 00:19:26', '2017-04-05 00:19:26'),
+  ('trn_tot_mu', 43, 'number', 'TRN Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('vandana_tot_mu', 52, 'number', 'VANDANA Total Generation MU', '2017-04-05 13:38:09', '2017-04-05 13:38:09'),
+  ('vstps_tot_mu', 32, 'number', 'VSTPS Generation MU', '2017-04-05 00:12:43', '2017-04-05 00:12:43');
 
 -- --------------------------------------------------------
 
@@ -124,8 +124,8 @@ CREATE TABLE IF NOT EXISTS `roles` (
 --
 
 INSERT INTO `roles` (`id`, `role_name`, `created_at`, `updated_at`) VALUES
-(1, 'WRCONS', '2017-04-02 21:08:15', '2017-04-02 21:08:15'),
-(2, 'ADMIN', '2017-04-02 21:08:15', '2017-04-02 21:08:15');
+  (1, 'WRCONS', '2017-04-02 21:08:15', '2017-04-02 21:08:15'),
+  (2, 'ADMIN', '2017-04-02 21:08:15', '2017-04-02 21:08:15');
 
 -- --------------------------------------------------------
 
@@ -147,36 +147,44 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `emailid` (`emailid`),
   KEY `role_id` (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `emailid`, `password`, `role_id`, `created_at`, `updated_at`, `is_verified`) VALUES
-(7, 'sudhir', 'nagasudhirpulla@gmail', 'pullasudhir', NULL, '2016-11-22 08:07:32', '2017-01-07 02:17:41', 1),
-(31, 'admin', 'nagasudh@j', 'abc123', 2, '2017-04-02 15:39:47', '2017-04-02 15:39:47', 1),
-(32, 'ntpc', 'ntpc@gmail.com', 'ntpc', 1, '2017-04-02 19:08:37', '2017-04-02 19:08:37', 0),
-(33, 'ssp', 'ssp@as', 'ssp', 1, '2017-04-04 09:38:24', '2017-04-04 09:38:24', 0),
-(34, 'kaps', 'kaps@as', 'kaps', 1, '2017-04-04 09:38:24', '2017-04-04 09:38:24', 0),
-(35, 'taps', 'taps@as', 'taps', 1, '2017-04-04 09:38:24', '2017-04-04 09:38:24', 0),
-(36, 'cgpl', 'cgpl@as', 'cgpl', 1, '2017-04-04 09:38:24', '2017-04-04 09:38:24', 0),
-(37, 'sasan', 'sasan@as', 'sasan', 1, '2017-04-04 09:38:24', '2017-04-04 09:38:24', 0),
-(38, 'nspcl', 'nspcl@as', 'nspcl', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
-(39, 'lanco', 'lanco@as', 'lanco', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
-(40, 'jpl', 'jpl@as', 'jpl', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
-(41, 'ksk', 'ksk@as', 'ksk', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
-(42, 'rkm', 'rkm@as', 'rkm', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
-(43, 'trn', 'trn@as', 'trn', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
-(44, 'mbpower', 'mbpower@as', 'mbpower', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
-(45, 'sks', 'sks@as', 'sks', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
-(46, 'jpltamnar', 'jpltamnar@as', 'jpltamnar', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
-(47, 'dgen', 'dgen@as', 'dgen', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
-(48, 'rgppl', 'rgppl@as', 'rgppl', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
-(49, 'jhabua', 'jhabua@as', 'jhabua', 1, '2017-04-04 15:39:33', '2017-04-04 15:39:33', 0),
-(50, 'balco', 'balco@as', 'balco', 1, '2017-04-04 15:39:33', '2017-04-04 15:39:33', 0),
-(51, 'essarmahan', 'essarmahan@as', 'essarmahan', 1, '2017-04-04 15:39:33', '2017-04-04 15:39:33', 0),
-(52, 'vandana', 'vandana@as', 'vandana', 1, '2017-04-04 15:39:33', '2017-04-04 15:39:33', 0);
+  (7, 'sudhir', 'nagasudhirpulla@gmail', 'pullasudhir', NULL, '2016-11-22 08:07:32', '2017-01-07 02:17:41', 1),
+  (31, 'admin', 'nagasudh@j', 'abc123', 2, '2017-04-02 15:39:47', '2017-04-02 15:39:47', 1),
+  (32, 'ntpc', 'ntpc@gmail.com', 'ntpc', 1, '2017-04-02 19:08:37', '2017-04-02 19:08:37', 0),
+  (33, 'ssp', 'ssp@as', 'ssp', 1, '2017-04-04 09:38:24', '2017-04-04 09:38:24', 0),
+  (34, 'kaps', 'kaps@as', 'kaps', 1, '2017-04-04 09:38:24', '2017-04-04 09:38:24', 0),
+  (35, 'taps', 'taps@as', 'taps', 1, '2017-04-04 09:38:24', '2017-04-04 09:38:24', 0),
+  (36, 'cgpl', 'cgpl@as', 'cgpl', 1, '2017-04-04 09:38:24', '2017-04-04 09:38:24', 0),
+  (37, 'sasan', 'sasan@as', 'sasan', 1, '2017-04-04 09:38:24', '2017-04-04 09:38:24', 0),
+  (38, 'nspcl', 'nspcl@as', 'nspcl', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
+  (39, 'lanco', 'lanco@as', 'lanco', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
+  (40, 'jpl', 'jpl@as', 'jpl', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
+  (41, 'ksk', 'ksk@as', 'ksk', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
+  (42, 'rkm', 'rkm@as', 'rkm', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
+  (43, 'trn', 'trn@as', 'trn', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
+  (44, 'mbpower', 'mbpower@as', 'mbpower', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
+  (45, 'sks', 'sks@as', 'sks', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
+  (46, 'jpltamnar', 'jpltamnar@as', 'jpltamnar', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
+  (47, 'dgen', 'dgen@as', 'dgen', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
+  (48, 'rgppl', 'rgppl@as', 'rgppl', 1, '2017-04-04 15:36:42', '2017-04-04 15:36:42', 0),
+  (49, 'jhabua', 'jhabua@as', 'jhabua', 1, '2017-04-04 15:39:33', '2017-04-04 15:39:33', 0),
+  (50, 'balco', 'balco@as', 'balco', 1, '2017-04-04 15:39:33', '2017-04-04 15:39:33', 0),
+  (51, 'essarmahan', 'essarmahan@as', 'essarmahan', 1, '2017-04-04 15:39:33', '2017-04-04 15:39:33', 0),
+  (52, 'vandana', 'vandana@as', 'vandana', 1, '2017-04-04 15:39:33', '2017-04-04 15:39:33', 0),
+  (53, 'cseb', 'cseb@gmail.com', 'cseb', 1, '2017-04-05 15:44:02', '2017-04-05 15:44:02', 0),
+  (54, 'mseb', 'mseb@gmail.com', 'mseb', 1, '2017-04-05 15:44:02', '2017-04-05 15:44:02', 0),
+  (55, 'mpseb', 'mpseb@gmail.com', 'mpseb', 1, '2017-04-05 15:44:02', '2017-04-05 15:44:02', 0),
+  (56, 'geb', 'geb@gmail.com', 'geb', 1, '2017-04-05 15:44:02', '2017-04-05 15:44:02', 0),
+  (57, 'dd', 'dd@gmail.com', 'dd', 1, '2017-04-05 15:44:02', '2017-04-05 15:44:02', 0),
+  (58, 'dnh', 'dnh@gmail.com', 'dnh', 1, '2017-04-05 15:44:02', '2017-04-05 15:44:02', 0),
+  (59, 'goa', 'goa@gmail.com', 'goa', 1, '2017-04-05 15:44:02', '2017-04-05 15:44:02', 0),
+  (60, 'esil', 'esil@gmail.com', 'esil', 1, '2017-04-05 15:44:02', '2017-04-05 15:44:02', 0);
 
 --
 -- Constraints for dumped tables
@@ -186,13 +194,13 @@ INSERT INTO `users` (`id`, `username`, `emailid`, `password`, `role_id`, `create
 -- Constraints for table `key_strings`
 --
 ALTER TABLE `key_strings`
-  ADD CONSTRAINT `key_strings_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `key_strings_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
